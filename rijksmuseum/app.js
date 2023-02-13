ad3558eaf0841944ecc3e7a87655a1227151958b
Nina Vens <nina.vens@hva.nl>
2023-02-13 18:35:15 +0100

    [cleaned]

--- rijksmuseum/app.js
+++
@@ -1,3 +0,0 @@
-const url = 'https://www.rijksmuseum.nl/api/nl/collection?key=RdKQCPfy&q=nachtwacht'
-const data = fetch(url).then(response => response.json())
-    .then(data => console.log(data));
