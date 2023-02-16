379056b5a9fc2b91b0c86463fc99016ea395fbfb
Nina Vens <nina.vens@hva.nl>
2023-02-16 17:51:51 +0100

    [changed] structure

--- rijksmuseum/scripts/randomArtData/displayRandomArt.js
+++ rijksmuseum/scripts/randomArtData/displayRandomArt.js
@@ -1,8 +1,8 @@
-export const displayRandomArt = (randomArtInfo) => {
-	const randomArtImg = document.querySelector('.generateRandomArt > section:first-of-type img');
-	const randomArtTitle = document.querySelector('.generateRandomArt > section:first-of-type h2');
-	const randomArtArtist = document.querySelector('.generateRandomArt > section:first-of-type > div:last-of-type > p');
+const randomArtImg = document.querySelector('.generateRandomArt > section:first-of-type img');
+const randomArtTitle = document.querySelector('.generateRandomArt > section:first-of-type h2');
+const randomArtArtist = document.querySelector('.generateRandomArt > section:first-of-type > div:last-of-type > p');
 
+export const displayRandomArt = (randomArtInfo) => {
 	randomArtImg.src = randomArtInfo.artImg + 1500;
 	randomArtImg.alt = randomArtInfo.artLongtitle;
 	randomArtTitle.textContent = randomArtInfo.artTitle;
