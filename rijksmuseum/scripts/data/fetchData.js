7ecb15aaf39e234bfe4624086a61ec594158e62c
Nina Vens <nina.vens@hva.nl>
2023-02-16 18:29:44 +0100

    [cleaned]

--- rijksmuseum/scripts/data/fetchData.js
+++ rijksmuseum/scripts/data/fetchData.js
@@ -4,30 +4,40 @@ import { searchResultsContainer } from '../../app.js';
 
 export const fetchData = async () => {
 	const userInput = document.querySelector('input[name="search"]').value;
-	if (userInput.length === 0) return;
-	startLoading();
-	const allArtObjects = document.querySelectorAll('section ul');
-	allArtObjects.forEach((artObject) => artObject.remove());
 	const searchErrorText = document.querySelector('.mainContent > section:nth-of-type(2) > div:first-of-type > p');
+
+	if (userInput.length === 0) return;
+
+	startLoading();
+
+	const allArtObjects = document.querySelectorAll('section ul');
+	for (const artObject of allArtObjects) artObject.remove();
+
 	searchErrorText.textContent = '';
 
 	const radioButtons = document.querySelectorAll('input[type="radio"]');
-
 	let radioValue;
 
-	radioButtons.forEach((radioButton) => {
-		if (radioButton.checked) radioValue = radioButton.value;
-		return radioValue;
-	});
+	for (const radioButton of radioButtons) {
+		if (radioButton.checked) {
+			radioValue = radioButton.value;
+		  	break;
+		}
+	}
 
-	let data;
 	const url = `https://www.rijksmuseum.nl/api/en/collection?key=RdKQCPfy&q=${userInput}&ps=${radioValue}&imgonly=true`;
+	let data;
 
 	try {
 		data = await (await fetch(url)).json();
+
 		stopLoading();
+
 		if (data.artObjects.length === 0) throw new Error();
-		data.artObjects.length < radioValue ? searchErrorText.textContent = `[${data.artObjects.length} pieces of art are being shown]` : searchErrorText.textContent = `[${radioValue} pieces of art are being shown]`
+		data.artObjects.length < radioValue
+			? (searchErrorText.textContent = `[${data.artObjects.length} pieces of art are being shown]`)
+			: (searchErrorText.textContent = `[${radioValue} pieces of art are being shown]`);
+			
 	} catch {
 		userInput.length > 11
 			? (searchErrorText.textContent =
