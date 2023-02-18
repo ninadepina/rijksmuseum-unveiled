ff61bf76e853838bb914d66ea5974c50e19ffa69
Nina Vens <nina.vens@hva.nl>
2023-02-18 12:56:05 +0100

    [fixed] img url

--- rijksmuseum/scripts/modules/data/getData.js
+++ rijksmuseum/scripts/modules/data/getData.js
@@ -3,7 +3,7 @@ import { displayData } from './displayData.js';
 export const getData = (artObjects) => {
 	if (artObjects.length === 0) return;
 	const artInfo = artObjects.map(({ webImage, longTitle, title, principalOrFirstMaker, id }) => ({
-		artImg: webImage,
+		artImg: webImage.url.slice(0, -3) + '=s1000',
 		artLongtitle: longTitle,
 		artTitle: title,
 		artArtist: principalOrFirstMaker,
