74d210b618b6e7991a353dde773502f5d6c0d6bc
Nina Vens <nina.vens@hva.nl>
2023-02-20 19:50:08 +0100

    [added] comments

--- rijksmuseum/scripts/modules/routing/routing.js
+++ rijksmuseum/scripts/modules/routing/routing.js
@@ -2,21 +2,25 @@ import { routes } from './routes.js';
 
 export const router = () => {
 	const path = window.location.hash.substring(1) || '/';
-	console.log(path);
 	const route = routes.find((_route) => {
+		// split the current path and route path into segments
 		const urlPathSegments = path.split('/').slice(1);
 		const routeSegments = _route.path.split('/').slice(1);
-		console.log(urlPathSegments, routeSegments);
+		// if the number of segments is different, the paths do not match
 		if (urlPathSegments.length !== routeSegments.length) return false;
 
 		const params = [];
+		// check each segment of the route
 		for (let i = 0; i < routeSegments.length; i++) {
+			// ff the segment starts with a colon(:), it is a parameter
 			if (routeSegments[i].startsWith(':')) {
+				// store the parameter value
 				params.push(urlPathSegments[i]);
 			} else if (routeSegments[i] !== urlPathSegments[i]) {
 				return false;
 			}
 		}
+		// call the view function of the matched route with the parameter
 		if (params.length > 0) {
 			_route.view(params[0]);
 		} else {
