b424d6387acdb4f3f1872b0bbf238b632b1aab3d
Nina Vens <nina.vens@hva.nl>
2023-02-21 17:15:09 +0100

    [added] better sorting

--- rijksmuseum/scripts/modules/routing/routes.js
+++ rijksmuseum/scripts/modules/routing/routes.js
@@ -86,7 +86,7 @@ const NormalView = () => {
 		fetchData();
 	});
 
-	function autocomplete(input, array) {
+	const autocomplete = (input, array) => {
 		let currentFocus;
 
 		input.addEventListener('input', function (e) {
@@ -95,10 +95,10 @@ const NormalView = () => {
 			let autocompleteList,
 				autocompleteItem,
 				i,
-				val = this.value;
+				value = this.value;
 
 			closeAllLists();
-			if (!val) return false;
+			if (!value) return false;
 			currentFocus = -1;
 
 			autocompleteList = document.createElement('div');
@@ -108,59 +108,66 @@ const NormalView = () => {
 			this.parentNode.appendChild(autocompleteList);
 
 			for (i = 0; i < array.length; i++) {
-				if (array[i].toUpperCase().indexOf(val.toUpperCase()) > -1) {
+				if (array[i].toUpperCase().indexOf(value.toUpperCase()) > -1) {
 					autocompleteItem = document.createElement('div');
 
-					let suggestion = array[i].replace(new RegExp(val, 'gi'), '<strong>$&</strong>');
-    				autocompleteItem.innerHTML = suggestion;
-    				autocompleteItem.innerHTML += "<input type='hidden' value='" + array[i] + "'>";
+					// checks if the input value is in the array item (if yes, highlights it)
+					let suggestion = array[i].replace(new RegExp(value, 'gi'), '<strong>$&</strong>');
+					autocompleteItem.innerHTML = suggestion;
+					autocompleteItem.innerHTML += "<input type='hidden' value='" + array[i] + "'>";
 
 					autocompleteItem.addEventListener('click', function (e) {
 						input.value = this.getElementsByTagName('input')[0].value;
 						closeAllLists();
 					});
-					autocompleteList.appendChild(autocompleteItem);
+
+					// sorts items in the list based on if they start with the input value
+					if (array[i].toUpperCase().startsWith(value.toUpperCase())) {
+						autocompleteList.insertBefore(autocompleteItem, autocompleteList.childNodes[0]);
+					} else {
+						autocompleteList.appendChild(autocompleteItem);
+					}
 				}
 			}
 		});
 
 		input.addEventListener('keydown', function (e) {
-			let x = document.getElementById(this.id + 'autocomplete-list');
+			let x = document.querySelector('#autocomplete-list');
 			if (x) x = x.getElementsByTagName('div');
 
-			if (e.keyCode == 40) { //down
+			if (e.key === 'ArrowDown') {
 				currentFocus++;
 				addActive(x);
-			} else if (e.keyCode == 38) { // up
+			} else if (e.key === 'ArrowUp') {
 				currentFocus--;
 				addActive(x);
-			} else if (e.keyCode == 13) { // enter
+			} else if (e.key === 'Enter') {
 				if (currentFocus > -1) {
 					if (x) x[currentFocus].click();
 				}
 			}
 		});
-		function addActive(x) {
+		const addActive = (x) => {
 			if (!x) return false;
 			removeActive(x);
 			if (currentFocus >= x.length) currentFocus = 0;
 			if (currentFocus < 0) currentFocus = x.length - 1;
 			x[currentFocus].classList.add('autocomplete-active');
-		}
-		function removeActive(x) {
+		};
+		const removeActive = (x) => {
 			for (let i = 0; i < x.length; i++) {
 				x[i].classList.remove('autocomplete-active');
 			}
-		}
-		function closeAllLists(elmnt) {
+		};
+		const closeAllLists = (elmnt) => {
 			const x = document.getElementsByClassName('autocomplete-items');
 			for (let i = 0; i < x.length; i++) {
 				if (elmnt != x[i] && elmnt != input) {
 					x[i].parentNode.removeChild(x[i]);
 				}
 			}
-		}
-	}
+		};
+	};
 	autocomplete(searchForm.querySelector('input'), suggestions);
 };
 
