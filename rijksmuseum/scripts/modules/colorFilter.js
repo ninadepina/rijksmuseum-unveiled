d735e041df66448a0c79c95c5cc8578a80761e46
Nina Vens <nina.vens@hva.nl>
2023-03-05 20:34:39 +0100

    [changed] color filter

---
+++ rijksmuseum/scripts/modules/colorFilter.js
@@ -0,0 +1,33 @@
+import { getData } from './data/getData.js';
+import { addColorFilterLoader, removeColorFilterLoader } from './loading.js';
+
+const fetchColorData = async (radioColorFilter) => {
+	const radioValueColorFilter = document.querySelector(`#${radioColorFilter.htmlFor}`).value.toUpperCase();
+	console.log(radioValueColorFilter);
+	const allArtObjects = document.querySelector('section ul');
+	const radioValueLanguage = localStorage.getItem('language');
+
+	let data;
+
+	const colorFilterLoaderSquares = document.querySelectorAll('.colorFilter > div > div');
+	colorFilterLoaderSquares.forEach((colorFilterLoaderSquare) => {
+		colorFilterLoaderSquare.style.backgroundColor = `#${radioValueColorFilter}`;
+	});
+
+	addColorFilterLoader();
+
+	if (allArtObjects) allArtObjects.remove();
+
+	const url = `https://www.rijksmuseum.nl/api/${radioValueLanguage}/collection?key=RdKQCPfy&ps=100&imgonly=true&f.normalized32Colors.hex=%20%23${radioValueColorFilter}`;
+
+	try {
+		data = await (await fetch(url)).json();
+		removeColorFilterLoader();
+		if (data.artObjects.length === 0) throw new Error();
+	} catch {
+		console.log('error');
+	}
+	getData(data.artObjects);
+};
+
+export { fetchColorData };
