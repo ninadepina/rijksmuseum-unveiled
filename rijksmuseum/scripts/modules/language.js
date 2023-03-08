c8573457e4115b0d4eaed0885ab9d8e73a4ab833
Nina Vens <nina.vens@hva.nl>
2023-03-08 22:22:23 +0100

    [fixed] changing template HTML

--- rijksmuseum/scripts/modules/language.js
+++ rijksmuseum/scripts/modules/language.js
@@ -1,7 +1,6 @@
 const generateRandomArtText = document.querySelector('.generateRandomArt button p');
 const generateRandomArtArrow = document.querySelector('.generateRandomArt button img');
 const openinghoursText = document.querySelector('.info p:first-of-type');
-const openinghoursTextMobile = document.querySelector('.staticInfo header div p:first-of-type');
 
 let language;
 
@@ -19,15 +18,23 @@ const currentLanguage = () => {
 };
 
 const staticInfo = () => {
-	if (localStorage.getItem('language') === 'nl') {
-		if (window.matchMedia('screen and (min-width: 1000px)').matches) {
-			generateRandomArtText.textContent = 'Genereer een willekeurig kunstwerk';
-			generateRandomArtArrow.alt =
-				'Icoon van een pijltje naar rechts voor het genereren van een willekeurig kunstwerk, klik op de knop om een willekeurig kunstwerk van de collectie te ontdekken.';
-			openinghoursText.textContent = 'Dagelijks 9 tot 17u';
-		} else {
-			openinghoursTextMobile.textContent = 'Dagelijks 9 tot 17u';
+	window.onload = () => {
+		if (localStorage.getItem('language') === 'nl') {
+			if (window.matchMedia('screen and (min-width: 1000px)').matches) {
+				generateRandomArtText.textContent = 'Genereer een willekeurig kunstwerk';
+				generateRandomArtArrow.alt =
+					'Icoon van een pijltje naar rechts voor het genereren van een willekeurig kunstwerk, klik op de knop om een willekeurig kunstwerk van de collectie te ontdekken.';
+				openinghoursText.textContent = 'Dagelijks 9 tot 17u';
+			} else {
+				changeInsertedHTML();
+			}
 		}
-	}
+	};
 };
+
+const changeInsertedHTML = () => {
+	const openinghoursTextMobile = document.querySelector('.staticInfo header div p:first-of-type');
+	openinghoursTextMobile.textContent = 'Dagelijks 9 tot 17u';
+};
+
 export { currentLanguage, staticInfo };
